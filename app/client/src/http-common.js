b9f9de21e154f9441bd8840841bfc50608b364ef
André Alexandre Gaio <aagaio@host-172-20-0-20.linwork.com.br>
2020-07-21 03:08:49 -0300

    General fixes about urls appointments in package.json and http-common.js in the client.

--- app/client/src/http-common.js
+++ app/client/src/http-common.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:3001/',
   // baseURL: 'https://desolate-reaches-33126.herokuapp.com/',
+  baseURL: 'https://aagaio-desafio-final.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
